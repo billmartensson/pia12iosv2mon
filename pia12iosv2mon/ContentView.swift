1b38c36d8fbca1f4cbd51ca032c8fe4135614a0e
Bill Mårtensson <bill@magictechnology.se>
2023-11-06 09:29:06 +0100

    Lagt till mer text

--- pia12iosv2mon/ContentView.swift
+++ pia12iosv2mon/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hej välden!")
+            Text("Mera text")
         }
         .padding()
     }
